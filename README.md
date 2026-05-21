d4ed5c6849156d3122cd42be795fbbd2de0471a2
pavlymba <146331202+pavlymba@users.noreply.github.com>
2026-05-21 20:41:43 +0000

    devops1

---
+++ README.md
@@ -0,0 +1,9 @@
+# js-fastify-blog
+
+## Запуск с Docker Compose
+
+1. Установите [Docker](https://www.docker.com/) и [Docker Compose](https://docs.docker.com/compose/).
+2. Выполните команду:
+
+   ```bash
+   docker-compose up
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+  return { message: 'Hello from Fastify Blog!' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
